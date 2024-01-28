3b84fa6c88bd561c4977e350852d7afd070a1188
Yashika <91620639+YashikaTanwar@users.noreply.github.com>
2024-01-28 15:29:05 +0530

    Update BlockCard.js

--- src/components/BlockCard/BlockCard.js
+++ src/components/BlockCard/BlockCard.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import {React,useState} from 'react'
 
 function BlockCard(props) {
+  const [isSelected,setisSelected]=useState(false);
+
   const addValuesToCategories = (value) => {
     const existingValue = props.categoryList.filter(
       (category) => category === value
@@ -10,16 +12,24 @@ function BlockCard(props) {
       props.setCategories([...props.categoryList, value]);
     }
     else {
-      //remove it from the array
+      const newCategoryList=props.categoryList.filter((category)=>category !== value);
+      props.setCategories(newCategoryList);
     }
   }
   return (
-    <div onClick={() => addValuesToCategories(props.genreDetails.id)}
+    <div 
+    onClick={() => {
+      addValuesToCategories(props.genreDetails.id);
+      // this will negate isSelected value i.e. unselect the value
+      setisSelected(!isSelected); 
+      props.RemoveCategory(props.genreDetails.id);
+    }}
       style={{
         background: props.genreDetails["color"],
         color: "white",
         padding: "16px",
-        borderRadius: "12px"
+        borderRadius: "12px",
+        border:`${isSelected ? "4px solid green":"4px solid white"}`
       }}
       key={props.Key}
     >
