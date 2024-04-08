927a064ed5096976b2a77830e47036b0810153ec
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-04-08 22:57:47 +0530

    capstone_project

---
+++ src/components/Weather/Weather.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect,useState} from 'react';
+import styles from '../Weather/Weather.module.css';
+const Weather=()=>{
+    const[date,setDate]=useState("");
+    const[time,setTime]=useState("");
+    const[weather,setWeather]=useState(false); 
+    return(
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
