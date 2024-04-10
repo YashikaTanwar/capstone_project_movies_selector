b8a12bdb7a5626af1a75d74990a18aa224c491e4
Yashika Tanwar <yashika.tanwar567@gmail.com>
2024-04-10 14:26:38 +0530

    capstone_project

--- src/apis/weather.js
+++ src/apis/weather.js
@@ -1,7 +1,8 @@
 import { Axios } from "axios";
+const DEAFULT_LOCATION="faridabad";
 export const getweatherDetails=async ()=>{
     try {
-        const reqUrl='http://api.weatherapi.com/v1/current.json?key=264a3e88da964890af3172939240904&q=faridabad';
+        const reqUrl=`http://api.weatherapi.com/v1/current.json?key=264a3e88da964890af3172939240904&q=${DEAFULT_LOCATION}`;
         const response=await Axios.get(reqUrl);
         console.log(response.data);
         return;   
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,14 +1,29 @@
 import React,{useEffect,useState} from 'react';
+import { getweatherDetails } from '../../apis/weather';
 import styles from '../Weather/Weather.module.css';
 const Weather=()=>{
     const[date,setDate]=useState("");
     const[time,setTime]=useState("");
     const[weather,setWeather]=useState(false); 
+
+    useEffect(()=>{
+        const fetchWeatherDetails=async()=>{
+
+        }
+    })
     return(
         <div>
             <div>
                 <div className={styles.container}>
                     <table className={styles.info}>
+                        <tr>
+                            <td>Date</td>
+                            <td>Time</td>
+                        </tr>
+                        <tr>
+                            <td>Date</td>
+                            <td>Time</td>
+                        </tr>
                         <tr>
                             <td>Date</td>
                             <td>Time</td>
